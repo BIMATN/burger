6ed1178523292e0e76cbef61b14570f222059637
Benjamin Rodriguez <bimatn@outlook.com>
2017-10-22 11:55:31 -0700

    database files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Monstrosity", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Shield", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hot Chipotle", false);
+
+SELECT*FROM burgers;
